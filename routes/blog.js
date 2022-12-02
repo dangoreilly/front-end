2a6f21e37d0b609fe54884f959b096f0a931bb0d
Dan O'Reilly <dangoreilly@gmail.com>
2022-12-02 17:43:56 +0000

    Added placeholder blog page

---
+++ routes/blog.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var router = express.Router();
+// const axios = require('axios');
+// const dotenv = require('dotenv').config();
+
+// let token = process.env.STRAPI_BEARER;
+
+// const config = {
+//   headers: { Authorization: `Bearer ${token}` }
+// };
+
+
+/* GET knights page. */
+router.get('/', function(req, res, next) {
+
+
+
+      // res.render('knights');
+});
+
+
+
+
+
+module.exports = router;
