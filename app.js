bcbc57fe7e378c91fb9ee2a815e6f2e5bd7b51ce
Dan O'Reilly <dangoreilly@gmail.com>
2023-01-09 03:07:52 +0000

    Added cookieparser

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-// var cookieParser = require('cookie-parser');
+var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const passport = require('passport');  // authentication
 const bodyParser = require('body-parser'); // parser middleware
@@ -25,7 +25,7 @@ app.set("view engine", "hjs");
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-// app.use(cookieParser());
+app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
