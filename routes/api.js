e56c4e674eb2131ea185f9b6761903dfa0d8d3d1
Dan O'Reilly <dangoreilly@gmail.com>
2023-01-29 16:14:57 +0000

    Added method to get Strapi URL from express

--- routes/api.js
+++ routes/api.js
@@ -5,6 +5,7 @@
 var express = require('express');
 var router = express.Router();
 const nebb = require("../modules/fixturesManagement")
+const dotenv = require('dotenv').config();
 
 // 
 router.get('/fixtures/:league', function(req, res, next) {
@@ -12,9 +13,10 @@ router.get('/fixtures/:league', function(req, res, next) {
     let leagueURL = req.params.league;
     Promise.resolve(nebb.getLeagueObject(leagueURL))
     .then((league) => {
-          // We don't need to verify that the fixtures object actually exists 
-          // because this shouldn't be called except from a pregenerated link 
-          res.json(league)
+        // We don't need to verify that the fixtures object actually exists 
+        // because this shouldn't be called except from a pregenerated link 
+        //console.log(league);
+        res.json(league)
     })
 
 });
@@ -25,8 +27,21 @@ router.get('/leagues', function(req, res, next) {
 
     Promise.resolve(nebb.getLeagues())
     .then((leagues) => {
-          res.json(leagues)
+        res.json(leagues)
     })
+
+});
+
+// Take the absolute address of the CMS from env and return it
+// Allows the management portal to connect to the CMS directly
+// instead of using the express server as a middleman
+router.get('/appinfo', function(req, res, next) {
+
+    let appinfo = {
+        "cms": process.env.STRAPI
+    }
+
+    res.json(appinfo)
 
 });
 
