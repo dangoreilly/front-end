e2e961868866c88eec989a6fe8c31e619f968715
Dan O'Reilly <dangoreilly@gmail.com>
2023-01-30 19:08:50 +0000

    Fixing various update methods

--- public/javascripts/fixtures-edit-vue.js
+++ public/javascripts/fixtures-edit-vue.js
@@ -10,14 +10,17 @@ var mainVueApp = {
         games_new:[],
         modified: false,
         strapi_cookie: "test",
+        STRAPI_BASE_URL: "", // For easy swapping between localhost testing and deployment address
 
     }),
   
     created() {
-      // TODO: Figure out why the cookie is being set to null after setting
-      this.strapi_cookie = this.getStrapiCookie();
+        // TODO: Figure out why the cookie is being set to null after setting
+        this.strapi_cookie = this.getStrapiCookie();
+        Promise.resolve(this.getStrapiURL())
+        .then((r) => {console.log("CMS address acquired")});
         Promise.resolve(this.fetchLeagues())
-        .then((r) => {console.log("Vue mounted")});
+        .then((r) => {console.log("League data fetched")});
     },
 
 
@@ -86,6 +89,23 @@ var mainVueApp = {
             }
 
             return count;
+        },
+        updateValid(){
+
+            // Cycle through the games list
+            // and new games list
+            // If any are invalid, the whole update is
+            // for(i = 0; i < this.games.length; i++){
+
+            //     if(!this.checkValidity(this.games[i])) return false;
+            // }
+            
+            for(i = 0; i < this.games_new.length; i++){
+
+                if(!this.checkValidity(this.games_new[i])) return false;
+            }
+
+            return true;
         }
 
     },
@@ -93,10 +113,10 @@ var mainVueApp = {
     methods: {
         async fetchLeagueData() {
             
-            console.log(`Fetching from ${this.leagueURL}`)
+            // console.log(`Fetching from ${this.leagueURL}`)
             let data = await (await fetch(this.leagueURL)).json()
             //Now that we have the data, parse it
-            console.log(data);
+            // console.log(data);
             this.league = data.league;
             this.games = data.games;
             this.games_clean = JSON.parse(JSON.stringify(data.games)); // For later comparisons for UI highlighting, deep copying is needed
@@ -143,7 +163,8 @@ var mainVueApp = {
             this.leagues.forEach(lg => {
 
                 if (lg.leagueName == this.selectedLeague){
-                    this.currentLeague = lg;
+                    // Deep copy league
+                    this.currentLeague = JSON.parse(JSON.stringify(lg))
                 } 
             });
 
@@ -151,12 +172,38 @@ var mainVueApp = {
 
         },
 
-        // getURL(){
-        //     let full_url = document.URL.split("/");
-        //     let relevant_URL = full_url[full_url.length-1];
-        //     this.leagueURL = "/fixtures/json/" + relevant_URL;
-        //     // console.log(`Fetching from ${this.leagueURL}`)
-        // },
+        reconcileTeams(game){
+            // When a teamName is updated, make the rest of the team attribute match it
+            game.homeTeam = this.matchTeamToTeamObject(game.homeTeam.teamName);
+            game.awayTeam = this.matchTeamToTeamObject(game.awayTeam.teamName);
+
+        },
+
+        matchTeamToTeamObject(_teamName){
+            // Takes in a teamName as a string
+            // Returns the corresponding Team Object
+
+            for(i = 0; i < this.teams.length; i++){
+            
+                if (_teamName == this.teams[i].teamName) {
+                    // Deep copy to avoid overwriting good data
+                    return JSON.parse(JSON.stringify(this.teams[i]));
+                }
+            }
+
+            // Return a null object if no match was found 
+            return {
+                "clubName": null,
+                "teamName": null,
+                "id": null
+            }
+        },
+
+        async getStrapiURL(){
+            let data = await (await fetch("/json/appinfo")).json()
+            this.STRAPI_BASE_URL = data.cms;
+            // console.log(`Fetching from ${this.leagueURL}`)
+        },
 
         getStrapiCookie(){
             // Reads the cookies in local storage, 
@@ -196,14 +243,42 @@ var mainVueApp = {
             return false;
 
         },
+
+        checkValidity(game){
+            // Check that key fields have been populated
+            // Doesn't really matter what the values are, just that the values exist
+            
+            //Assume game is invalid
+            game.valid = false;
+
+            if(!game.ISO_date) return false;
+            if(!game.homeTeam.teamName) return false;
+            // if(!game.homeScore) return false;
+            // if(!game.homePoints) return false;
+            if(!game.awayTeam.teamName) return false;
+            // if(!game.awayScore) return false;
+            // if(!game.awayPoints) return false;
+            
+            // Add a flag for the UI to check
+            game.valid = true;
+            return true
+
+        },
+
         addNewFixture(){
             //Add a new fixture, held in a different 
             let newFixture = {
                 "ISO_date": null,
-                "homeTeam": "",
+                "homeTeam": {
+                    "clubName": null,
+                    "teamName": null
+                },
                 "homeScore": null,
                 "homePoints": null,
-                "awayTeam": "",
+                "awayTeam": {
+                    "clubName": null,
+                    "teamName": null
+                },
                 "awayScore": null,
                 "awayPoints": null,
                 "posted": true
@@ -237,14 +312,14 @@ var mainVueApp = {
         // Display fixture review screen
         // When user sends clicks send, start sending and update UI to show send/failed/pending
 
-        sendUpdates(){
+        async sendUpdates(){
 
             // Send POST request
-            this.POSTnewFixtures();
+            await this.POSTnewFixtures();
             // Send PUT request
-            this.PUTupdatedFixtures();
+            await this.PUTupdatedFixtures();
             // Send DELETE request
-            this.deleteFixturesFromServer();
+            await this.deleteFixturesFromServer();
             // Report to the user
             window.alert("Fixtures updated");
             // Refresh the UI
@@ -254,7 +329,7 @@ var mainVueApp = {
             // console.log("sendUpdates()");
 
         },
-        deleteFixturesFromServer(){
+        async deleteFixturesFromServer(){
             // Collect an array of fixtures to be deleted
             // This could be done live, but it's easier to do it here
             let toBeDeleted = [];
@@ -270,9 +345,10 @@ var mainVueApp = {
             // For each game that's getting deleted, send a delete request in a promise. 
             // We'll resolve them all later
             for( i = 0; i < toBeDeleted.length; i ++){
-                // TODO: Update CMS link
-                deletedGames[i] = Promise.resolve( 
-                    fetch(`http://localhost:1337/api/fixtures/${toBeDeleted[i].id}`, {
+
+                deletedGames[i] =  await fetch(
+                    `${this.STRAPI_BASE_URL}/api/fixtures/${toBeDeleted[i].id}`, 
+                    {
                         method: 'DELETE',
                         headers: {
                             'Accept': 'application/json',
@@ -280,43 +356,102 @@ var mainVueApp = {
                             "Authorization": `Bearer ${this.getStrapiCookie()}`,
 
                         }
-                    })
-                );
-            }
+                    }
+                )
 
-            Promise.all(deletedGames)
-            .then(deletedGames_resolved => {
-                // Now that we have responses for all our DELETEs, check how many were actually successful
-                for( i = 0; i < deletedGames_resolved.length; i ++){
-                    if (deletedGames_resolved[i].ok){
-                        console.log(`Deleted Fixture ${deletedGames_resolved[i].id}`);
-                        deleted++;
-                    }
-                    else {
-                        // If a fixture fails to be deleted, throw an alert to the user with the 
-                        // fixture ID that failed to delete. 
-                        console.log("Deletion Failed");
-                        console.log(deletedGames_resolved[i]);
-                        window.alert(`Failed to delete Fixture ${deletedGames_resolved[i].id}`)
-                    }
+                if (deletedGames[i].ok){
+                    console.log(`Deleted Fixture ${toBeDeleted[i].id}`);
+                    Modifed++;
+                }
+                else {
+                    // If a fixture fails to be deleted, throw an alert to the user with the 
+                    // fixture ID that failed to delete. 
+                    console.log(`DELETE Failed - ${toBeDeleted[i].id}`);
                 }
 
-                window.alert(`Deleted ${deleted} fixtures`)
-            })
+            }
 
+            //When finished, Print succesful number of deletes
+            window.alert(`Deleted ${deleted} fixtures`)
+            let timestamp = new Date(Date.now()).toISOString()
+            console.log(`${timestamp}: Deleted ${deleted} fixtures`);
 
-
-            //When finished, Print succesful number of deletes to console
-            console.log(`Deleted ${deleted} fixtures`);
         },
-        POSTnewFixtures(){
+        async PUTupdatedFixtures(){
+            //Send updates to the server           
+            
+            // Collect an array of fixtures to be modifed
+            // This could be done live, but it's easier to do it here
+            let toBeModified = [];
+            let ModifedGames = [];
+            let Modifed = 0;
+
+            this.games.forEach((game) => {
+                if (game.modified){
+                    toBeModified.push(game);
+                }
+            });
+
+            // For each game that's getting modifed, create a Promise with that new fixture. 
+            // We'll resolve them all later
+            for( i = 0; i < toBeModified.length; i++){
+                //TODO: Update Strapi Link
+                ModifedGames[i] = await fetch(
+                    `${this.STRAPI_BASE_URL}/api/fixtures/${toBeModified[i].id}`, 
+                    {
+                        method: 'PUT',
+                        headers: {
+                            'Accept': 'application/json',
+                            'Content-Type': 'application/json',
+                            "Authorization": `Bearer ${this.getStrapiCookie()}`,
+
+                        },
+                        // We don't need to send all the fields, strapi would interpret
+                        // a PUT with incomplete data to only update the specified fields
+                        // but it's easier to just send the full lot
+                        body: JSON.stringify(this.packageFixture(toBeModified[i]))
+                    }
+                )
+                
+                if (ModifedGames[i].ok){
+                    console.log(`Updated Fixture ${ModifedGames[i].id}`);
+                    Modifed++;
+                }
+                else {
+                    // If a fixture fails to be deleted, throw an alert to the user with the 
+                    // fixture ID that failed to delete. 
+                    console.log(`PUT Failed - ${ModifedGames[i].id}`);
+                }
+
+            }
+
+            //When finished, Print succesful number of deletes
+            window.alert(`Modified ${Modifed} fixtures`)
+            let timestamp = new Date(Date.now()).toISOString()
+            console.log(`${timestamp}: Modified ${Modifed} fixtures`);
+        },
+        async POSTnewFixtures(){
             //Send new fixtures to the server
             console.log("POSTnewFixtures()");
         },
-        PUTupdatedFixtures(){
-            //Send updates to the server
-            console.log("PUTupdatedFixtures()");
-        },
+
+        packageFixture(f){
+            // Takes in a fixture object
+            // Transforms to have the appropriate fingerprint for Strapi to consume
+
+            return {
+                "data": {
+                    "Date": f.ISO_date,
+                    "homeTeamScore": f.homeScore,
+                    "awayTeamScore": f.awayScore,
+                    "awayTeamPointsAwarded": f.awayPoints,
+                    "homeTeamPointsAwarded": f.homePoints,
+                    "posted": true,
+                    "team": f.homeTeam.id,
+                    "awayTeam": f.awayTeam.id
+                }
+            }
+        }
 
     }
   }
